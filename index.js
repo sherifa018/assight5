37a69a597225c1ede4aa57d92a16028b47bf4057
sherifa <sherifaalsaiqe@gmail.com>
2024-10-23 12:07:56 -0500

    first comment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    function add(a, b) {
+      return a + b;
+    }
+    let add = (a, b) => a + b;
+
+    function multiply(a, b) {
+      return a * b;
+    }
+    let multiply = (a, b) => a * b;
+
+    function greet(name) {
+      return "hello," + name;
+    }
+    let greet = (name) => "hello," + name;
+  
